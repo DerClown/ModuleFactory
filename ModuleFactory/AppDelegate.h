eb9c0d3b2a5ea30f313534033139f0d11ec41615
DerClown <839235027@qq.com>
2017-12-31 01:42:41 +0800

    init

---
+++ ModuleFactory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ModuleFactory
+//
+//  Created by YoYo on 2017/12/31.
+//  Copyright © 2017年 YoYo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ModuleFactory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModuleFactory
+//
+//  Created by YoYo on 2017/12/31.
+//  Copyright © 2017年 YoYo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
